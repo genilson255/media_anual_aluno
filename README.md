7069c9ff82a01d805ee43f16be9a4e5a967b1263
Genilson <genilson255@hotmail.com>
2024-02-10 10:32:52 -0300

    Add os inputs com as medias de 1 ao 4

--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Media aluno ("Python")
 # 3 Se a media do aluno for maior que 6 e menor ou 7, aparece uma mensagem dizendo: 'Sua media esta abaixo do esperado'
 # 4 Se a media for sete ou menor ou igual a nove, "Voce esta aprovado"
 # Se a media for igual a 10, parabens voce conseguiu aprovação com 100% de aprovação
+
+# (As entradas devem ser do tipo int)
--- media.py
+++ media.py
@@ -1,1 +1,8 @@
-print("Hello, world")
+b1 = int(input('Informe a media do primeiro bimestre: '))
+b2 = int(input('Informe a media do segundo bimestre: '))
+b3 = int(input('Informe a media do terceiro bimestre: '))
+b4 = int(input('Informe a media do quarto bimestre: '))
+
+media = (b1 + b2 + b3 + b4) / 4
+print('A soma das medias {}, {}, {}, {} É: Media: {}'
+      .format(b1, b2, b3, b4, media))
